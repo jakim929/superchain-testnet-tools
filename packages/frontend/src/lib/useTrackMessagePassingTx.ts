00b007dea206d71fe098e0d6d0de5ad80da0c837
James Kim <jakim929@gmail.com>
2023-10-31 11:26:53 -0400

    remove unused function

--- packages/frontend/src/lib/useTrackMessagePassingTx.ts
+++ packages/frontend/src/lib/useTrackMessagePassingTx.ts
@@ -1,9 +1,9 @@
 import { Chain, Hex } from 'viem'
 import { useWaitForTransaction } from 'wagmi'
 
-export const useTrackMessagePassingTx = (l1Chain: Chain, txHash?: Hex) => {
-  const { isLoading, data } = useWaitForTransaction({
-    hash: txHash,
-    confirmations: 5,
-  })
-}
+// export const useTrackMessagePassingTx = (l1Chain: Chain, txHash?: Hex) => {
+//   const { isLoading, data } = useWaitForTransaction({
+//     hash: txHash,
+//     confirmations: 5,
+//   })
+// }
